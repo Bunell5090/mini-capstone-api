33809e01b21d4b38fccc7e0e0750906b773b2f37
Bunell5090 <Bunell5090.com>
2022-03-08 20:50:02 -0600

    Updated Routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  get "/get_product", controller: "products", action: "get_product_method"
+  get "/get_one_product", controller: "products", action: "get_product_method"
+  get "/get_all_products", controller: "products", action: "all_products_method"
 end
