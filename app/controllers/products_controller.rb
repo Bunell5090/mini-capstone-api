ba9540bc8de0b2365afadb55bc7eb64fa3e86d99
Bunell5090 <Bunell5090.com>
2022-03-10 18:47:17 -0600

    Added methods to products controller and updasted routes to match

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,26 @@
 class ProductsController < ApplicationController
-
-
-  def get_product
-    
+  def all_products_method
+    my_products = Product.all
+    render json: my_products.as_json
   end
+
+  def first_product_method
+    first_product = Product.first
+    render json: first_product.as_json
+  end
+
+  def second_product_method
+    second_product = Product.second
+    render json: second_product.as_json
+  end
+
+  def third_product_method
+    third_product = Product.third
+    render json: third_product.as_json
+  end
+
+
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get "/get_one_product" => "products#get_product_method"
   get "/get_all_products" => "products#all_products_method"
+  get "/first_product" => "products#first_product_method"
+  get "/second_product" => "products#second_product_method"
+  get "/third_product" => "products#third_product_method"
 end
