89f33020bb3409611ed4b3ac2e786bf2247ab7eb
Bunell5090 <Bunell5090.com>
2022-03-13 10:54:25 -0500

    removed prdocut methods & routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,28 +1,4 @@
 class ProductsController < ApplicationController
-  def get_one_product
-    
-  end
-  
-  def all_products_method
-    my_products = Product.all
-    render json: my_products.as_json
-  end
-
-  def first_product_method
-    first_product = Product.first
-    render json: first_product.as_json
-  end
-
-  def second_product_method
-    second_product = Product.second
-    render json: second_product.as_json
-  end
-
-  def third_product_method
-    third_product = Product.third
-    render json: third_product.as_json
-  end
-
   def index
     products = Product.all
     render json: products.as_json
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get "/get_one_product" => "products#get_product_method"
-  get "/get_all_products" => "products#all_products_method"
-  get "/first_product" => "products#first_product_method"
-  get "/second_product" => "products#second_product_method"
-  get "/third_product" => "products#third_product_method"
+
   get "/products" => "products#index"
   get "/products/:id" => "products#show"
 end
