033e270dc871a46050790faa9cce10a8ef254fe3
Bunell5090 <Bunell5090.com>
2022-03-13 11:44:26 -0500

    Added Update & Create http requests to request.http

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "/products" => "products#index"
   get "/products/:id" => "products#show"
-  get "/product/:id" => "product#show"
+  get "/product/:id" => "product#create"
   patch "/product/:id" => "product#update"
 end
