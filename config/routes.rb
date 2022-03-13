0216529d6698cd6b8cfc7a4e3d1a8c4d44a08668
Bunell5090 <Bunell5090.com>
2022-03-13 11:41:35 -0500

    Added Create & Update Routes to Routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get "/products" => "products#index"
   get "/products/:id" => "products#show"
+  get "/product/:id" => "product#show"
+  patch "/product/:id" => "product#update"
 end
