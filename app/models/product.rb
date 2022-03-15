4cc8142bd91ad0cde429bf0ff868fe3c6ddfbb23
Bunell5090 <Bunell5090.com>
2022-03-14 19:08:14 -0500

    Added methods to app/models/product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,18 @@
 class Product < ApplicationRecord
+  def is_discounted?
+   if price < 10
+    return true
+   else
+    return false
+  end
+
+  def tax
+    tax = price * .09
+    return tax
+  end
+
+  def total
+    total = price * 1.09
+  end
+
 end
