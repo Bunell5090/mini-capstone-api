b17e3ccbff524ec0a3f2ca2105abc708164595fe
Bunell5090 <Bunell5090.com>
2022-03-17 20:53:18 -0500

    Updated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
+
+  #products
   get "/products" => "products#index"
   get "/products/:id" => "products#show"
   post "/products/:id" => "products#create"
   patch "/products/:id" => "products#update"
   delete "/products/:id" => "products#destroy"
+  
+  #suppliers
   get "/suppliers" => "suppliers#index"
-  get "/suppliers/:id" => "suppliers#show"
-  post "/suppliers/:id" => "suppliers#create"
-  patch "/suppliers/:id" => "suppliers#update"
-  delete "/suppliers/:id" => "suppliers#destroy"
 end
