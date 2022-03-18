b2b8c49a5485a23e797d9db89094c16ae2919e13
Bunell5090 <Bunell5090.com>
2022-03-17 19:53:13 -0500

    Added Supplier Routes & validations to models/supplier

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,2 +1,10 @@
 class Supplier < ApplicationRecord
+  validates :name, presence: true 
+  validates :name, uniqueness: true
+  validates :email, presence: true 
+  validates :phone_number, presence: true 
+  validates :phone_number, length: 10
+
+
+
 end
