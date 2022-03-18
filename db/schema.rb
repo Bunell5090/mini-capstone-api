3022bfeafc62b1e252cc490bd379871330005fcb
Bunell5090 <Bunell5090.com>
2022-03-17 19:22:15 -0500

    Added Supplier model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_16_231953) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_18_002113) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_16_231953) do
     t.string "rating"
     t.integer "cost"
     t.integer "quantity"
+  end
+
+  create_table "suppliers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
