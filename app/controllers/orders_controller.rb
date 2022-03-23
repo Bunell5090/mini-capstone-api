ec7c99fccfcea761e0d0b6fa9b1a14fa4a48b376
Bunell5090 <Bunell5090.com>
2022-03-22 19:26:20 -0500

    Added Order controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,17 @@
 class OrdersController < ApplicationController
+  def create
+    product = Product.new(
+      user_id: params["user_id"], 
+      product_id: params["product_id"], 
+      quantity: params["quantity"], 
+      subtotal: params["subtotal"],
+      tax: params["tax"],
+      total: pramas["total"]
+    )
+    if product.save #happy path
+      render json: product
+    else #sad path
+      render json: {error_messages: product.errors.full_messages}, status: 422
+    end
+  end
 end
