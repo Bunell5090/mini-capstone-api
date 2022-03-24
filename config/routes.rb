40acdd8fbf246860943ad8bd0130137d431c6330
Bunell5090 <Bunell5090.com>
2022-03-24 16:32:54 -0500

    Created order serializer & added attributes & relationships

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   #products
   get "/products" => "products#index"
   get "/products/:id" => "products#show"
-  post "/products/:id" => "products#create"
+  post "/products/" => "products#create"
   patch "/products/:id" => "products#update"
   delete "/products/:id" => "products#destroy"
   
