3fc5387aa6092a53ed737ec78fc2fcc722d2e06a
Bunell5090 <Bunell5090.com>
2022-03-26 11:53:47 -0500

    Removed image & order serializer due to conflicts

--- app/serializers/image_serializer.rb
+++
@@ -1,4 +0,0 @@
-class ImageSerializer < ActiveModel::Serializer
-  attributes :id, :product_id, :url
-  belongs_to :products
-end
--- app/serializers/order_serializer.rb
+++
@@ -1,5 +0,0 @@
-class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :product_id, :quantity
-
-  has_many :products
-end
